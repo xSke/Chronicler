fb612be5c0d15de36817a43a3b4ac69d35935f96
Astrid <me@astr.cc>
2020-09-24 23:46:40 +0200

    Fix migrations

--- SIBR.Storage.Data/Schema/r__player_team_views.sql
+++ SIBR.Storage.Data/Schema/r__player_team_views.sql
@@ -1,4 +1,6 @@
-﻿drop materialized view if exists current_roster;
+﻿drop materialized view if exists players;
+drop materialized view if exists player_versions;
+drop materialized view if exists current_roster;
 drop materialized view if exists team_versions;
 drop materialized view if exists teams;
 
@@ -29,3 +31,28 @@ create materialized view current_roster as
             from jsonb_array_elements_text(data -> position) with ordinality
         ) as players on true;
 create unique index on current_roster (player_id);
+
+create materialized view player_versions as
+    select
+        entity_id as player_id,
+        first_seen,
+        last_seen,
+        hash,
+        data
+    from versions_view
+    where type = 1; -- Player = 1
+create unique index player_versions_pkey on player_versions (player_id, first_seen);
+
+create materialized view players as
+    select distinct on (player_id)
+        player_id,
+        first_seen as timestamp,
+        hash,
+        data,
+        team_id,
+        position,
+        roster_index
+    from player_versions
+         inner join current_roster using (player_id)
+    order by player_id, first_seen desc;
+create unique index players_pkey on players (player_id);
