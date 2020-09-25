1a5d269311b1d1d5dd14bc5ee73693a13b7e80f0
Astrid <me@astr.cc>
2020-09-25 16:54:43 +0200

    add pg function for partially recalculating game_updates_unique table

---
+++ SIBR.Storage.Data/Schema/r__recalculate_unique_games.sql
@@ -0,0 +1,29 @@
+﻿drop function if exists recalculate_game_updates_for(game_id uuid[]);
+create or replace function recalculate_game_updates_for(game_id uuid[]) returns int as
+$$
+declare
+    count int;
+begin
+    delete from game_updates_unique gu where gu.game_id = any(recalculate_game_updates_for.game_id);
+    insert into game_updates_unique
+        select
+            hashes.hash,
+            first.game_id,
+            timestamp,
+            data,
+            season,
+            day,
+            null
+        from (
+            select distinct hash from game_updates gu
+            where gu.game_id = any(recalculate_game_updates_for.game_id)
+        ) hashes
+        inner join lateral (
+            select * from game_updates gu where gu.hash = hashes.hash order by timestamp limit 1
+        ) as first on true
+        inner join objects on hashes.hash = objects.hash;
+    
+    get diagnostics count = row_count;
+    return count;
+end;
+$$ language plpgsql;
