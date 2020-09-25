8a7d019734b6311b534f7a795a5df60c3092d3b6
Astrid <me@astr.cc>
2020-09-25 21:38:29 +0200

    Add incinerated players import script

--- SIBR.Storage.Data/DataSources.cs
+++ SIBR.Storage.Data/DataSources.cs
@@ -10,5 +10,6 @@ namespace SIBR.Storage.Data
         public static readonly Guid ImmaterialServer = new Guid("b7a2f88f-1945-427b-b435-082af9c4d48e");
         public static readonly Guid ImmaterialAstridLocal = new Guid("3735c017-b670-4411-8a09-624a76e9ead5");
         public static readonly Guid BlaseballApiOldGames = new Guid("a4715d03-d092-4ef4-a3cc-4a19776a6fd5");
+        public static readonly Guid BlaseballApiOldPlayers = new Guid("c57920eb-dcca-438b-bdc6-b0ca3deb0368");
     }
 }
---
+++ SIBR.Storage.Data/Scripts/import_incinerated_players.py
@@ -0,0 +1,25 @@
+﻿import json
+from datetime import datetime
+
+import requests
+
+print("Fetching tributes")
+tributes = requests.get("https://www.blaseball.com/api/getTribute").json()
+player_ids = [tribute["playerId"] for tribute in tributes]
+
+print("Fetching players")
+players = requests.get("https://www.blaseball.com/database/players?ids=" + ",".join(player_ids)).json()
+timestamp = datetime.utcnow()
+
+updates = []
+for player in players:
+    updates.append({
+        "type": 1,
+        "timestamp": timestamp.isoformat() + "Z",
+        "data": player
+    })
+
+print("Inserting player updates")
+source_id = "c57920eb-dcca-438b-bdc6-b0ca3deb0368"
+url = "http://localhost:4011/internal/updates?source={}".format(source_id)
+resp = requests.post(url, data=json.dumps(updates), headers={"Content-Type": "application/json"})
