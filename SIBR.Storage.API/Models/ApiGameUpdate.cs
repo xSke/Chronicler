ec13c99e252f89331ed52f22f9de353ae196dcbb
Astrid <me@astr.cc>
2020-10-05 10:54:54 +0200

    Add hash field back to game update

--- SIBR.Storage.API/Models/ApiGameUpdate.cs
+++ SIBR.Storage.API/Models/ApiGameUpdate.cs
@@ -9,7 +9,7 @@ namespace SIBR.Storage.API.Models
     {
         public Guid GameId { get; set; }
         public Instant Timestamp { get; set; }
-        public Guid Hash;
+        public Guid Hash { get; set; }
         public JsonElement Data { get; set; }
 
         public ApiGameUpdate(GameUpdateView db)
