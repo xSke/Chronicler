4ce6b71d54d8c477c637d52b5d75945c5889aea6
Astrid <me@astr.cc>
2020-10-19 11:22:11 +0200

    Add new migration

---
+++ SIBR.Storage.Data/Schema/v13__add_entities_last_update.sql
@@ -0,0 +1,30 @@
+﻿alter table entities add column last_update uuid;
+
+create or replace function on_updates_insert_entities_trg() returns trigger as 
+$$
+    begin
+        insert into entities (type, entity_id, last_update)
+            values (new.type, new.entity_id, new.update_id)
+            on conflict do nothing;
+            
+        update entities e
+            set last_update = new.update_id
+            where
+                e.type = new.type and
+                e.entity_id = new.entity_id and
+                new.timestamp > (select timestamp from updates u where u.update_id = e.last_update);
+                
+        return new;
+    end;
+$$ language plpgsql;
+
+update entities e
+    set last_update = (
+        select update_id
+        from updates u
+            where u.type = e.type and u.entity_id = e.entity_id 
+        order by timestamp desc limit 1
+    )
+    where e.last_update is null;
+    
+alter table entities alter column last_update set not null;
